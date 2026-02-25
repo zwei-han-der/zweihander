7ee94238080edbeec02be34f5ffc4579c5075287
Zweihander <gustavodev@duck.com>
2026-02-25 10:31:46 -0300

    feat: adicionado algum conteúdo para a aba home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
 import Default from "../layouts/Default";
+import "../styles/Home.css";
 
 function Home() {
-  return <Default>gostoso</Default>;
+  return (
+    <Default>
+      <div className="main-content">
+        <p className="p-content">
+          Olá, eu sou o Gustavo e essa é a minha página principal. Ainda está
+          deserta, mas tente interagir com o player de música ali no canto
+          superior esquerdo.
+        </p>
+        <p className="p-content">Ou ver minhas incríveis jogadas na aba de <Link to="/videos">vídeos</Link>.</p>
+      </div>
+    </Default>
+  );
 }
 
 export default Home;
