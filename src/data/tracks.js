aff24d19a4c9fa0e3c6a9021f92c99ef918dc08d
Zweihander <gustavodev@duck.com>
2026-02-25 00:38:57 -0300

    feat: adicionadas novas musicas

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -1,19 +1,43 @@
 import diamanteslagrimaserostosparaesquecer from "../assets/images/diamantes-lagrimas-e-rostos-para-esquecer.jpg";
 import abaixodoradar from "../assets/images/abaixo-do-radar.jpg";
+import thefirsttime from "../assets/images/the-first-time.jpg";
+import chromakopia from "../assets/images/chromakopia.jpg";
+import produtodoambiente from "../assets/images/produto-do-ambiente.jpg";
 
 export const trackList = [
+  {
+    name: "Amém, Amém (Deekapz Remix)",
+    artist: "BK & Deekapz",
+    album: "PRODUTO DO AMBIENTE",
+    cover: produtodoambiente,
+    link: "https://file.garden/aZ5b6SvMW2wrHr27/agsdryu.m4a",
+  },
   {
     name: "Não Adianta Chorar",
     artist: "BK & JXNV$",
     album: "Diamantes, Lágrimas e Rostos para esquecer",
     cover: diamanteslagrimaserostosparaesquecer,
-    link: "https://files.catbox.moe/nyppyf.mp3",
+    link: "https://file.garden/aZ5b6SvMW2wrHr27/nyppyf.mp3",
   },
   {
     name: "ABAIXO DO RADAR",
     artist: "Febem, CESRV & Smile",
     album: "ABAIXO DO RADAR",
     cover: abaixodoradar,
-    link: "https://files.catbox.moe/ekl7fd.mp3",
+    link: "https://file.garden/aZ5b6SvMW2wrHr27/ekl7fd.mp3",
+  },
+  {
+    name: "NIGHTS LIKE THIS",
+    artist: "The Kid LAROI",
+    album: "THE FIRST TIME",
+    cover: thefirsttime,
+    link: "https://file.garden/aZ5b6SvMW2wrHr27/vnsjgo.mp3",
+  },
+  {
+    name: "Like Him",
+    artist: "Tyler, The Creator",
+    album: "CHROMAKOPIA",
+    cover: chromakopia,
+    link: "https://file.garden/aZ5b6SvMW2wrHr27/vmk9qj.m4a",
   },
 ];
