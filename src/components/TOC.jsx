6bf0126747718d3436e29d656b35567b11c4db79
Zweihander <gustavodev@duck.com>
2026-02-24 21:52:03 -0300

    feat: componente de navegação

---
+++ src/components/TOC.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+import "../styles/TOC.css";
+
+function TOC() {
+  return (
+    <div className="toc">
+      <h2>Páginas</h2>
+      <div className="list">
+        <Link to="/" className="list-item">
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M3 13h1v7c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-7h1c.4 0 .77-.24.92-.62.15-.37.07-.8-.22-1.09l-8.99-9a.996.996 0 0 0-1.41 0l-9.01 9c-.29.29-.37.72-.22 1.09s.52.62.92.62Zm7 7v-5h4v5zm2-15.59 6 6V20h-2v-5c0-1.1-.9-2-2-2h-4c-1.1 0-2 .9-2 2v5H6v-9.59z"></path>
+            </svg>
+          </span>
+          <p>HOME</p>
+        </Link>
+        <Link to="/changelog" className="list-item">
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="m19.94 7.68-.03-.09a.8.8 0 0 0-.2-.29l-5-5c-.09-.09-.19-.15-.29-.2l-.09-.03a.8.8 0 0 0-.26-.05c-.02 0-.04-.01-.06-.01H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-12s-.01-.04-.01-.06c0-.09-.02-.17-.05-.26ZM6 20V4h7v4c0 .55.45 1 1 1h4v11z"></path>
+            </svg>
+          </span>
+          <p>CHANGELOG</p>
+        </Link>
+        <Link to="/videos" className="list-item">
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m0 4h-2V5h2zM7 11H5V9h2zm0 2v2H5v-2zm2 0h6v6H9zm0-2V5h6v6zm8 2h2v2h-2zm0-2V9h2v2zM7 5v2H5V5zM5 17h2v2H5zm12 2v-2h2v2z"></path>
+            </svg>
+          </span>
+          <p>Vídeos</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default TOC;
