a3e6631fb869c3b6b15fb1166b7261ed2a304584
Zweihander <gustavodev@duck.com>
2026-02-25 09:34:15 -0300

    chore: e volta o cão arrependido

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/zweihander">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/changelog" element={<Changelog />} />
