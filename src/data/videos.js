0af88b9a0992ceda702a6bf7fb59c20ea1a99719
Zweihander <gustavodev@duck.com>
2026-02-25 11:34:05 -0300

    refactor: atualziada ordem dos videos para mais recente primeiro

--- src/data/videos.js
+++ src/data/videos.js
@@ -18,40 +18,10 @@ import thumb_23_02_2026_2 from "../assets/videos/thumbnails/23-02-2026-2.jpg";
 
 export const videos = [
     {
-        title: "primeira bola de 3 no dunk",
-        date: "01/12/2025",
-        video: video_01_12_2025_1,
-        poster: thumb_01_12_2025_1
-    },
-    {
-        title: "pump fake",
-        date: "03/11/2025",
-        video: video_03_11_2025_1,
-        poster: thumb_03_11_2025_1
-    },
-    {
-        title: "midrangemaster 1.0",
-        date: "09/02/2026",
-        video: video_09_02_2026_1,
-        poster: thumb_09_02_2026_1
-    },
-    {
-        title: "midrangemaster 2.0",
-        date: "13/10/2025",
-        video: video_13_10_2025_1,
-        poster: thumb_13_10_2025_1
-    },
-    {
-        title: "tava quente esse dia",
-        date: "13/10/2025",
-        video: video_13_10_2025_2,
-        poster: thumb_13_10_2025_2
-    },
-    {
-        title: "midrangemaster 3.0",
-        date: "10/02/2026",
-        video: video_20_02_2026_1,
-        poster: thumb_20_02_2026_1
+        title: "hustle",
+        date: "23/02/2026",
+        video: video_23_02_2026_2,
+        poster: thumb_23_02_2026_2
     },
     {
         title: "midrangemaster 4.0",
@@ -60,9 +30,39 @@ export const videos = [
         poster: thumb_23_02_2026_1
     },
     {
-        title: "hustle",
-        date: "23/02/2026",
-        video: video_23_02_2026_2,
-        poster: thumb_23_02_2026_2
+        title: "midrangemaster 3.0",
+        date: "10/02/2026",
+        video: video_20_02_2026_1,
+        poster: thumb_20_02_2026_1
+    },
+    {
+        title: "tava quente esse dia",
+        date: "13/10/2025",
+        video: video_13_10_2025_2,
+        poster: thumb_13_10_2025_2
+    },
+    {
+        title: "midrangemaster 2.0",
+        date: "13/10/2025",
+        video: video_13_10_2025_1,
+        poster: thumb_13_10_2025_1
+    },
+    {
+        title: "midrangemaster 1.0",
+        date: "09/02/2026",
+        video: video_09_02_2026_1,
+        poster: thumb_09_02_2026_1
+    },
+    {
+        title: "pump fake",
+        date: "03/11/2025",
+        video: video_03_11_2025_1,
+        poster: thumb_03_11_2025_1
+    },
+    {
+        title: "primeira bola de 3 no dunk",
+        date: "01/12/2025",
+        video: video_01_12_2025_1,
+        poster: thumb_01_12_2025_1
     },
 ]
