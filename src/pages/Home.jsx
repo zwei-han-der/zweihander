b2c75a290644929974ca619ef7c28ba8ab41eddb
Zweihander <gustavodev@duck.com>
2026-02-25 17:05:13 -0300

    refactor: mini ajustes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,12 @@ function Home() {
     <Default>
       <div className="main-content">
         <p className="p-content">
-          Olá, eu sou o Gustavo e essa é a minha página principal. Ainda está
-          deserta, mas tente interagir com o player de música ali no canto
+          Olá, eu sou o Gustavo e este é o meu site. Ainda está
+          deserto, mas tente interagir com o player de música ali no canto
           superior esquerdo.
         </p>
         <p className="p-content">Ou ver minhas incríveis jogadas na aba de <Link to="/videos">vídeos</Link>.</p>
+        <p>Tente também dar F5 na página 😉</p>
       </div>
     </Default>
   );
