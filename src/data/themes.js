b1b004b74ffa169e3df5aa95afedb135179cc029
Zweihander <gustavodev@duck.com>
2026-02-24 21:51:45 -0300

    feat: adiciona scrollbar personalizado e temas aleatorios

---
+++ src/data/themes.js
@@ -0,0 +1,62 @@
+export const themes = [
+  {
+    color01: "#200f21",
+    color02: "#382039",
+    color03: "#5a3d5c",
+    color04: "#f638dc",
+  },
+  {
+    color01: "#4f3961",
+    color02: "#ea728c",
+    color03: "#fc9d9d",
+    color04: "#a89696",
+  },
+  {
+    color01: "#fc4100",
+    color02: "#00215e",
+    color03: "#ffc55a",
+    color04: "#2c4e80",
+  },
+  {
+    color01: "#6f4a8e",
+    color02: "#221f3b",
+    color03: "#050505",
+    color04: "#ebebeb",
+  },
+  {
+    color01: "#ba135d",
+    color02: "#f4cca4",
+    color03: "#d99879",
+    color04: "#000000",
+  },
+  {
+    color01: "#1a2634",
+    color02: "#203e5f",
+    color03: "#ffcc00",
+    color04: "#fee5b1",
+  },
+  {
+    color01: "#1a2849",
+    color02: "#505bda",
+    color03: "#b063c5",
+    color04: "#ffaac3",
+  },
+  {
+    color01: "#ea047e",
+    color02: "#ff6d28",
+    color03: "#d8b4f8",
+    color04: "#00f5ff",
+  },
+  {
+    color01: "#e966a0",
+    color02: "#6554af",
+    color03: "#9575de",
+    color04: "#2b2730",
+  },
+  {
+    color01: "#125c13",
+    color02: "#3e7c17",
+    color03: "#d7c0ae",
+    color04: "#e8e1d9",
+  },
+];
