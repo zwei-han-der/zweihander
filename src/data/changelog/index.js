300b63925826b5608701539d08efe4054c21b89b
Zweihander <gustavodev@duck.com>
2026-02-27 00:16:29 -0300

    feat: começada a implementação de suporte ao markdown

---
+++ src/data/changelog/index.js
@@ -0,0 +1,43 @@
+import v105 from '../../data/markdown/changelog/v1.0.5.md?raw';
+import v104 from '../../data/markdown/changelog/v1.0.4.md?raw';
+import v103 from '../../data/markdown/changelog/v1.0.3.md?raw';
+import v102 from '../../data/markdown/changelog/v1.0.2.md?raw';
+import v101 from '../../data/markdown/changelog/v1.0.1.md?raw';
+
+export const logs = [
+    {
+        title: "boookkkkmmmmaaarrrkkkkssss",
+        version: "changelog #5",
+        date: "25/02/2026",
+        description: "Adicionada a aba de bookmarks. Ainda não está 100% completa, mas tá lá.",
+        content: v105
+    },
+    {
+        title: "marquee é legal demais",
+        version: "changelog #4",
+        date: "25/02/2026",
+        description: "Gostei tanto que mudei a animação de marquee no overflow do texto para um utilitário próprio.",
+        content: v104
+    },
+    {
+        title: "anotações são a base da recordação",
+        version: "changelog #3",
+        date: "25/02/2026",
+        description: "Changelog adicionado, pronto para receber atualizações do site.",
+        content: v103
+    },
+    {
+        title: "recordar é viver",
+        version: "changelog #2",
+        date: "24/02/2026",
+        description: "Aba de vídeos pronta, feita para armazenar, principalmente, minhas jogadas no basquete.",
+        content: v102
+    },
+    {
+        title: "música é vida",
+        version: "changelog #1",
+        date: "23/02/2026",
+        description: "Primeira feature do site adicionada, player de música completamente funcional.",
+        content: v101
+    },
+];
