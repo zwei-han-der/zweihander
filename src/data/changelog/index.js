95136dba26f0bce5ac3b5154ea97cd9e3187bc73
Zweihander <gustavodev@duck.com>
2026-02-28 15:21:03 -0300

    chore: reestruturação do versionamento

--- src/data/changelog/index.js
+++ src/data/changelog/index.js
@@ -1,59 +1,67 @@
-import v106 from '../../data/changelog/v1.0.6.md?raw';
-import v105 from '../../data/changelog/v1.0.5.md?raw';
-import v104 from '../../data/changelog/v1.0.4.md?raw';
-import v103 from '../../data/changelog/v1.0.3.md?raw';
-import v102 from '../../data/changelog/v1.0.2.md?raw';
-import v101 from '../../data/changelog/v1.0.1.md?raw';
-import teste from '../../data/changelog/teste.md?raw';
+import v016 from '../../data/changelog/v0.1.6.md?raw';
+import v015 from '../../data/changelog/v0.1.5.md?raw';
+import v014 from '../../data/changelog/v0.1.4.md?raw';
+import v013 from '../../data/changelog/v0.1.3.md?raw';
+import v012 from '../../data/changelog/v0.1.2.md?raw';
+import v011 from '../../data/changelog/v0.1.1.md?raw';
+import v100 from '../../data/changelog/v1.0.0.md?raw';
+import todo from '../../data/changelog/todo.md?raw';
 
 export const logs = [
     {
-        title: "teste markdown",
-        version: "temporário",
+        title: "lista de afazeres",
+        version: "📌 fixado",
         date: "27/02/2026",
-        description: "Teste markdown",
-        content: teste
+        description: "Aqui ficará arquivado tudo o que já foi feito, o que está sendo feito e o que será feito no site.",
+        content: todo
+    },
+    {
+        title: "Zweihander v1.0.0",
+        version: "changelog #7",
+        date: "28/02/2026",
+        description: "Primeira versão estável.",
+        content: v100
     },
     {
         title: "refazendo changelog",
         version: "changelog #6",
         date: "27/02/2026",
         description: "Changelog agora com modal.",
-        content: v106
+        content: v016
     },
     {
         title: "boookkkkmmmmaaarrrkkkkssss",
         version: "changelog #5",
         date: "25/02/2026",
         description: "Adicionada a aba de bookmarks. Ainda não está 100% completa, mas tá lá.",
-        content: v105
+        content: v015
     },
     {
         title: "marquee é legal demais",
         version: "changelog #4",
         date: "25/02/2026",
         description: "Gostei tanto que mudei a animação de marquee no overflow do texto para um utilitário próprio.",
-        content: v104
+        content: v014
     },
     {
         title: "anotações são a base da recordação",
         version: "changelog #3",
         date: "25/02/2026",
         description: "Changelog adicionado, pronto para receber atualizações do site.",
-        content: v103
+        content: v013
     },
     {
         title: "recordar é viver",
         version: "changelog #2",
         date: "24/02/2026",
         description: "Aba de vídeos pronta, feita para armazenar, principalmente, minhas jogadas no basquete.",
-        content: v102
+        content: v012
     },
     {
         title: "música é vida",
         version: "changelog #1",
         date: "23/02/2026",
         description: "Primeira feature do site adicionada, player de música completamente funcional.",
-        content: v101
+        content: v011
     },
 ];
---
+++ src/data/changelog/todo.md
@@ -0,0 +1,17 @@
+Aqui ficará arquivado tudo o que já foi feito, o que está sendo feito e o que será feito no site. Ultima atualização em: **28/08/2026**
+
+## Planejado
+
+- Adicionar mais tópicos no [Bookmarks](/zweihander/bookmarks)
+- Adicionar aba de blog
+- Adicionar botão shuffle e possibilidade de reordenar a ordem das músicas que irão tocar
+
+## Em progresso
+
+## Completo
+
+- Suporte a Markdown e Syntax Highlight
+- Aba [Bookmarks](/zweihander/bookmarks)
+- Aba [Changelog](/zweihander/changelog)
+- Aba [Vídeos](/zweihander/videos)
+- Player de música com playlist própria
