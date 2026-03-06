e8aa489ab811ad1bfc413a73a44bd2c5c5df96fb
Zweihander <gustavodev@duck.com>
2026-03-06 10:30:34 -0300

    chore: adicionado novos vídeos

--- src/data/videos.js
+++ src/data/videos.js
@@ -7,6 +7,9 @@ import video_2026_02_20_1 from "../assets/videos/2026-02-20-1.mp4";
 import video_2026_02_23_1 from "../assets/videos/2026-02-23-1.mp4";
 import video_2026_02_23_2 from "../assets/videos/2026-02-23-2.mp4";
 import video_2026_02_26_1 from "../assets/videos/2026-02-26-1.mp4";
+import video_2026_03_05_1 from "../assets/videos/2026-03-05-1.mp4";
+import video_2026_03_05_2 from "../assets/videos/2026-03-05-2.mp4";
+import video_2026_03_05_3 from "../assets/videos/2026-03-05-3.mp4";
 
 import thumb_2025_12_01_1 from "../assets/videos/thumbnails/2025-12-01-1.jpg";
 import thumb_2025_11_03_1 from "../assets/videos/thumbnails/2025-11-03-1.jpg";
@@ -17,60 +20,81 @@ import thumb_2026_02_20_1 from "../assets/videos/thumbnails/2026-02-20-1.jpg";
 import thumb_2026_02_23_1 from "../assets/videos/thumbnails/2026-02-23-1.jpg";
 import thumb_2026_02_23_2 from "../assets/videos/thumbnails/2026-02-23-2.jpg";
 import thumb_2026_02_26_1 from "../assets/videos/thumbnails/2026-02-26-1.jpg";
+import thumb_2026_03_05_1 from "../assets/videos/thumbnails/2026-03-05-1.jpg";
+import thumb_2026_03_05_2 from "../assets/videos/thumbnails/2026-03-05-2.jpg";
+import thumb_2026_03_05_3 from "../assets/videos/thumbnails/2026-03-05-3.jpg";
 
 export const videos = [
-    {
-        title: "midrangemaster 5.0",
-        date: "26/02/2026",
-        video: video_2026_02_26_1,
-        poster: thumb_2026_02_26_1
-    },
-    {
-        title: "midrangemaster 4.0",
-        date: "23/02/2026",
-        video: video_2026_02_23_1,
-        poster: thumb_2026_02_23_1
-    },
-    {
-        title: "hustle",
-        date: "23/02/2026",
-        video: video_2026_02_23_2,
-        poster: thumb_2026_02_23_2
-    },
-    {
-        title: "midrangemaster 3.0",
-        date: "20/02/2026",
-        video: video_2026_02_20_1,
-        poster: thumb_2026_02_20_1
-    },
-    {
-        title: "midrangemaster 2.0",
-        date: "09/02/2026",
-        video: video_2026_02_09_1,
-        poster: thumb_2026_02_09_1
-    },
-    {
-        title: "choradinha",
-        date: "01/12/2025",
-        video: video_2025_12_01_1,
-        poster: thumb_2025_12_01_1
-    },
-    {
-        title: "pump fake",
-        date: "03/11/2025",
-        video: video_2025_11_03_1,
-        poster: thumb_2025_11_03_1
-    },
-    {
-        title: "tava quente esse dia",
-        date: "13/10/2025",
-        video: video_2025_10_13_2,
-        poster: thumb_2025_10_13_2
-    },
-    {
-        title: "midrangemaster 1.0",
-        date: "13/10/2025",
-        video: video_2025_10_13_1,
-        poster: thumb_2025_10_13_1
-    },
-]
+  {
+    title: "contestadíssimo",
+    date: "05/03/2026",
+    video: video_2026_03_05_3,
+    poster: thumb_2026_03_05_3,
+  },
+  {
+    title: "midrangemaster 6.0",
+    date: "05/03/2026",
+    video: video_2026_03_05_2,
+    poster: thumb_2026_03_05_2,
+  },
+  {
+    title: "fastbreak",
+    date: "05/03/2026",
+    video: video_2026_03_05_1,
+    poster: thumb_2026_03_05_1,
+  },
+  {
+    title: "midrangemaster 5.0",
+    date: "26/02/2026",
+    video: video_2026_02_26_1,
+    poster: thumb_2026_02_26_1,
+  },
+  {
+    title: "midrangemaster 4.0",
+    date: "23/02/2026",
+    video: video_2026_02_23_1,
+    poster: thumb_2026_02_23_1,
+  },
+  {
+    title: "hustle",
+    date: "23/02/2026",
+    video: video_2026_02_23_2,
+    poster: thumb_2026_02_23_2,
+  },
+  {
+    title: "midrangemaster 3.0",
+    date: "20/02/2026",
+    video: video_2026_02_20_1,
+    poster: thumb_2026_02_20_1,
+  },
+  {
+    title: "midrangemaster 2.0",
+    date: "09/02/2026",
+    video: video_2026_02_09_1,
+    poster: thumb_2026_02_09_1,
+  },
+  {
+    title: "choradinha",
+    date: "01/12/2025",
+    video: video_2025_12_01_1,
+    poster: thumb_2025_12_01_1,
+  },
+  {
+    title: "pump fake",
+    date: "03/11/2025",
+    video: video_2025_11_03_1,
+    poster: thumb_2025_11_03_1,
+  },
+  {
+    title: "tava quente esse dia",
+    date: "13/10/2025",
+    video: video_2025_10_13_2,
+    poster: thumb_2025_10_13_2,
+  },
+  {
+    title: "midrangemaster 1.0",
+    date: "13/10/2025",
+    video: video_2025_10_13_1,
+    poster: thumb_2025_10_13_1,
+  },
+];
